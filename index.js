72474b3ff07802c2a93ba5a9132285eec4fe46fe
iamso <so@dev.so>
2024-02-02 14:30:10 +0100

    catch some errors and handle quitting

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 const prompts = require('prompts')
+const kleur = require('kleur')
 
 async function run () {
-  const { stdout: branches } = await exec('git branch -v --sort=-committerdate')
+  // exit if this is not a git repo
+  try {
+    await exec('git rev-parse --git-dir > /dev/null 2>&1')
+  } catch(err) {
+    logError('Not a git repo')
+    process.exit(1)
+  }
 
+  // get branches
+  const { stdout: branches } = await exec('git branch -v --sort=-committerdate')
   const choices = branches
     .split(/\n/)
     .filter(branch => !!branch.trim())
@@ -15,6 +24,12 @@ async function run () {
       return { value, hint, disabled: flag === '*' }
     })
 
+  // exit if there isn't more than 1 branch
+  if (choices.length <= 1) {
+    logError('No branches to select')
+    process.exit(1)
+  }
+
   const { branch } = await prompts({
     type: 'select',
     name: 'branch',
@@ -22,8 +37,9 @@ async function run () {
     choices,
     hint: choices[0].hint,
     warn: 'current branch',
-    onState ({ value }) {
-      this.hint = choices.find(c => c.value === value).hint
+    onState (state) {
+      this.hint = choices.find(c => c.value === state.value).hint
+      onAbort(state)
     }
   })
 
@@ -37,6 +53,25 @@ async function checkout (branch) {
   process.stderr.write(stderr)
 }
 
+function logSuccess(text, newLine = false) {
+  newLine && console.log()
+  console.log(kleur.green().bold(text))
+}
+
+function logError(text, newLine = false) {
+  newLine && console.log()
+  console.log(kleur.red().bold(text))
+}
+
+function onAbort(state) {
+  if (state.aborted) {
+    process.nextTick(() => {
+      logSuccess('Ok bye \\_(-_-)_/', true)
+      process.exit(0);
+    })
+  }
+}
+
 function onError (e) {
   if (e.stderr) {
     process.stderr.write(e.stderr)
@@ -46,3 +81,7 @@ function onError (e) {
 }
 
 run().catch(onError)
+
+process.on('SIGINT', () => {
+  onAbort({ aborted: true })
+});
